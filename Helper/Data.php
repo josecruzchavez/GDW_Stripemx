a02011b13ce53afbf9305f0c97d1d044e5ce1e97
José Cruz Chávez <josecruzchavez10@gmail.com>
2026-05-23 19:46:12 -0600

    refactor: migrate Stripemx to GDW Core 4 metadata and CSP

--- Helper/Data.php
+++ Helper/Data.php
@@ -1,8 +1,20 @@
 <?php
+declare(strict_types=1);
 
 namespace GDW\Stripemx\Helper;
 
 class Data extends \GDW\Core\Helper\Data
 {
-    const GDW_MODULE_CODE = 'payment/gdw_stripemx/';
+    private const GDW_MODULE_CODE = 'payment/gdw_stripemx/';
+
+    public function getModuleCode(): string
+    {
+        return self::GDW_MODULE_CODE;
+    }
+
+    /** @return mixed */
+    public function getDirectVal(string $field)
+    {
+        return $this->getConfigValue($this->getModuleCode() . $field);
+    }
 }
